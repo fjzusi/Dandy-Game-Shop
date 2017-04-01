76dd26074856325c17b28f087d02eb9e5e16c992
joedono <zidane320@yahoo.com>
2017-04-01 14:40:18 -0400

    Continuing angular tutorial

--- js/main.js
+++ js/main.js
@@ -3,6 +3,14 @@ var app = angular.module('myApp', []);
 app.controller("myCtrl", function($scope) {
 	$scope.firstName = "Lord";
 	$scope.lastName = "Joe";
+	
+	$scope.myColor = "blue";
+});
+
+app.directive("myShoppingCart", function() {
+	return {
+		template: "<select><option>1</option><option>2</option></select>"
+	}
 });
 
 app.config([
