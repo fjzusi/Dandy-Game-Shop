9c49e75df1ca77638c2ae7c9b20da0776604c6d8
fjzusi <zidane320@yahoo.com>
2017-04-25 18:04:29 -0400

    Worked on individual item view

--- js/controllers/gameController.js
+++ js/controllers/gameController.js
@@ -2,16 +2,14 @@ app.controller('gameController',
 	['$scope', '$routeParams', 'gamesService', 'cartService',
 	function ($scope, $routeParams, gamesService, cartService) {
 		var id = $routeParams.id;
-		
+		$scope.game = {};
+
 		gamesService
 			.getGameDetails(id)
 			.then(function (data) {
-				$scope.id = data.id;
-				$scope.title = data.title;
-				$scope.subtitle = data.subTitle;
-				$scope.price = data.price;
+				$scope.game = data;
 			});
-			
+
 		$scope.addToCart = function(gameId, quantity) {
 			cartService.addToCart(gameId, quantity);
 		}
