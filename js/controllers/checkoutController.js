ae6755683f9fa92d6e11ab01022ccae1ed09564f
fjzusi <zidane320@yahoo.com>
2017-04-29 12:34:35 -0400

    Started working on the checkout page

--- js/controllers/checkoutController.js
+++ js/controllers/checkoutController.js
@@ -1,13 +1,16 @@
 app.controller('checkoutController',
 	['$scope', '$rootScope', '$location', 'cartService',
 	function ($scope, $rootScope, $location, cartService) {
+		cartService.addToCart(3, 1);
+		cartService.addToCart(7, 2);
+		cartService.addToCart(13, 1);
 
-        $scope.$watch(
+		$scope.$watch(
 			"cartEmpty",
 			function onCartEmptyChange(oldValue, newValue) {
-                if(oldValue) {
-                    $location.url("/");
-                }
+				if(oldValue) {
+					//$location.url("/");
+				}
 			}
 		);
 	}]
